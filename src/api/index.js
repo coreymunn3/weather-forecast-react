31a4a190a06729d7b719581477f102cfa94f4298
Michael Corey Munn <coreymunn3@gmail.com>
2020-11-19 10:58:32 -0500

    set up weather context and started writing getCurrentAndForecast method to pull weather data from API

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+// forecast example (includes current data)
+// http://api.weatherapi.com/v1/forecast.json?key=edc6002a55784169bff151603201811&q=New_York&days=3
+// history example (one day at a time)
+// http://api.weatherapi.com/v1/history.json?key=edc6002a55784169bff151603201811&q=London&dt=2020-11-16
+
+// api key
+// edc6002a55784169bff151603201811
---
+++ src/context/weather/WeatherState.js
@@ -0,0 +1,57 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import weatherContext from './WeatherContext';
+import weatherReducer from './weatherReducer';
+// import types
+
+const ContactState = (props) => {
+  // 1. initial state
+  const initialState = {
+    location: null,
+    currentWeather: null,
+    forecastWeather: null,
+    historyWeather: null,
+    weatherAlert: null,
+    selectedDayWeather: null,
+    error: null,
+  };
+  // 2. activate useReducer hook
+  const { state, dispatch } = useReducer(weatherReducer, initialState);
+
+  // pull out API key
+  weatherApiKey = process.env.WEATHER_API_KEY;
+
+  // 3. Methods
+  const getCurrentAndForecast = async (location) => {
+    try {
+      const res = await axios.get(
+        `http://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${location}&days=3`
+      );
+      // dispatch
+    } catch (error) {
+      // error.response.msg
+    }
+  };
+
+  // get historical
+
+  // 4. Return the provider
+  return (
+    <weatherContext.Provider
+      value={{
+        location: state.location,
+        currentWeather: state.currentWeather,
+        forecastWeather: state.forecastWeather,
+        historyWeather: state.historyWeather,
+        weatherAlert: state.weatherAlert,
+        selectedDayWeather: state.selectedDayWeather,
+        error: state.error,
+        getCurrentAndForecast,
+      }}
+    >
+      {props.children}
+    </weatherContext.Provider>
+  );
+};
+
+export default ContactState;
