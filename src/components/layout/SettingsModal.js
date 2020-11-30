84976700881a17792d1ea47d698024b2f3240db2
Michael Corey Munn <coreymunn3@gmail.com>
2020-11-29 22:41:46 -0500

    put settingsModal inside of a container for better spacing on medium large screens, added CSS to modal-content class to be 80% of container on all screen sizes, giving better modal appearance on mobile screens.

--- src/components/layout/SettingsModal.js
+++ src/components/layout/SettingsModal.js
@@ -23,49 +23,50 @@ const SettingsModal = ({ modalActive, toggleModal }) => {
   };
 
   return (
-    <div className={`modal ${modalActive ? 'is-active' : ''}`}>
-      <div className='modal-background'></div>
-      <div className='modal-content'>
-        <div className='box'>
-          <h2 className='title is-4 has-text-black has-text-centered'>
-            Settings
-          </h2>
+    <div className='container'>
+      <div className={`modal ${modalActive ? 'is-active' : ''}`}>
+        <div className='modal-background'></div>
+        <div className='modal-content'>
+          <div className='box'>
+            <h2 className='title is-4 has-text-black has-text-centered'>
+              Settings
+            </h2>
 
-          <div className='field is-horizontal'>
-            <div className='field-label'>
-              <label className='label'>City</label>
-            </div>
-            <div className='field-body'>
-              <div className='field'>
-                <p className='control is-expanded'>
-                  <input
-                    className='input is-small'
-                    type='text'
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                  />
-                </p>
+            <div className='field is-horizontal'>
+              <div className='field-label'>
+                <label className='label'>City</label>
+              </div>
+              <div className='field-body'>
+                <div className='field'>
+                  <p className='control is-expanded'>
+                    <input
+                      className='input is-small'
+                      type='text'
+                      value={city}
+                      onChange={(e) => setCity(e.target.value)}
+                    />
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='field is-horizontal'>
-            <div className='field-label'>
-              <label className='label'>State</label>
-            </div>
-            <div className='field-body'>
-              <div className='field'>
-                <p className='control is-expanded'>
-                  <input
-                    className='input is-small'
-                    type='text'
-                    value={state}
-                    onChange={(e) => setState(e.target.value)}
-                  />
-                </p>
+            <div className='field is-horizontal'>
+              <div className='field-label'>
+                <label className='label'>State</label>
+              </div>
+              <div className='field-body'>
+                <div className='field'>
+                  <p className='control is-expanded'>
+                    <input
+                      className='input is-small'
+                      type='text'
+                      value={state}
+                      onChange={(e) => setState(e.target.value)}
+                    />
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-          {/* <div className='field is-horizontal'>
+            {/* <div className='field is-horizontal'>
               <div className='field-label is-small'>
                 <label className='label'>Measurements</label>
               </div>
@@ -82,28 +83,29 @@ const SettingsModal = ({ modalActive, toggleModal }) => {
                 </div>
               </div>
             </div> */}
-          <div className='field is-horizontal'>
-            <div className='field-label is-small'></div>
-            <div className='field-body'>
-              <div className='field'>
-                <p className='control'>
-                  <button
-                    className='button is-primary is-small is-fullwidth'
-                    onClick={() => handleSubmit()}
-                  >
-                    Save
-                  </button>
-                </p>
+            <div className='field is-horizontal'>
+              <div className='field-label is-small'></div>
+              <div className='field-body'>
+                <div className='field'>
+                  <p className='control'>
+                    <button
+                      className='button is-primary is-small is-fullwidth'
+                      onClick={() => handleSubmit()}
+                    >
+                      Save
+                    </button>
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <button
+          className='modal-close is-large'
+          aria-label='close'
+          onClick={toggleModal}
+        ></button>
       </div>
-      <button
-        className='modal-close is-large'
-        aria-label='close'
-        onClick={toggleModal}
-      ></button>
     </div>
   );
 };
